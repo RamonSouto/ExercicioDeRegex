c43e6bbf0587d916ce3d2ec0b3cf55959d4c39ce
RamonSouto <rdssi@hotmail.com>
2023-10-11 21:06:35 -0300

    05 - Quantificadores Zero ou Mais

---
+++ 05Quantificadores/ZeroMais.js
@@ -0,0 +1,9 @@
+const texto1 = 'De longe eu avistei fogo e uma pessoa grtou: FOGOOOOOO!'
+const texto2 = 'There is a big fog in NYC'
+
+// * → zero ou mais
+const regex = /fogo*/gi
+console.log(texto1.match(regex));
+console.log(texto2.match(regex));
+
+
