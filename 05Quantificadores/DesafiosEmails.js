2953d9bf60fa546282510d9cc2198973ca5b6037
RamonSouto <rdssi@hotmail.com>
2023-10-11 23:26:04 -0300

    05 - Quantificadores Desafio emails

--- 05Quantificadores/DesafiosEmails.js
+++ 05Quantificadores/DesafiosEmails.js
@@ -1,1 +1,14 @@
-const texto = `Os e-mails dos convidados são: `
+const texto = `
+Os e-mails dos convidados são:
+	- fulano@cod3er.com.br
+	- xico@gmail.com
+	- joao@empresa.info.br
+	- maria_silva@registro.br
+	- rafa.sampaio@yahoo.com`
+
+console.log(texto.match(/[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,4}/g));
+console.log(texto.match(/\w+@\w+\.\w{2,4}/g));
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}/g));
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g));
+
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}(\.\w{2})?/g));
